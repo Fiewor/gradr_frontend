d0dc9207c4a3808f2548f7ce28d121bde0fd238b
Jubril Oshisanwo <folajubril@gmail.com>
2024-04-30 18:43:47 +0100

    Delete gradr-frontend directory

--- gradr-frontend/src/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div className="flex  bg-[#FFFFFF] items-center justify-end min-h-[80px] p-[20px]">
-<p >Header</p>
-    </div>
-  )
-}
--- gradr-frontend/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Assesments from './assessments';
-
-export interface Props {}
-
-const HomeOverview= () => <Assesments />;
-
-export default HomeOverview;
